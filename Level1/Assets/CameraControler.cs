43e8efe3f5fcd544c1519d154769777c9f45c1f3
Jose Ramon Garcia Gonzalez <ram00ngar@gmail.com>
2017-11-04 10:24:59 -0600

    Nivel 1

---
+++ Level1/Assets/CameraControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+   
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+        
+    }
+
+	void LateUpdate () {
+        //transform.Rotate(new Vector3(Input.acceleration.x * -1, 0, Input.acceleration.y * -1) * Time.deltaTime * 20);
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Level1/Assets/escena.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class escena : MonoBehaviour {
+    float speed,x,y,z;
+	// Use this for initialization
+	void Start () {
+        speed = 40;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //Debug.Log(x);
+        transform.Rotate(new Vector3(Input.acceleration.x*-1,0, Input.acceleration.y*-1) *Time.deltaTime*speed);
+        //transform.Rotate(Input.gyro.attitude);
+        /*
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime * speed, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back* Time.deltaTime * speed, Space.World);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * speed, Space.World);
+        }*/
+
+    }
+
+}
